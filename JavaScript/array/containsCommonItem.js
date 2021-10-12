e2cbff9025db7884f6ecbe17fe43041b385694d5
Riddhi Damani <riddhidamani90@gmail.com>
2021-10-11 20:02:23 -0500

    Common Items JS Problem

--- JavaScript/array/containsCommonItem.js
+++ JavaScript/array/containsCommonItem.js
@@ -8,3 +8,47 @@
 
 // 2 parameters - arrays - no size limit
 // return true or false
+
+const array1 = ["a", "b", "c", "x"];
+const array2 = ["z", "y", "a"];
+
+// Brute Force - Naive Solution
+// Big O(a * b)  - Not efficient, readable.
+//O(1) - Space Complexity
+// First approach
+function containsCommonItem(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//O(a + b) Time Complexity
+//O(a) Space Complexity
+// Second approach
+function containsCommonItem2(arr1, arr2) {
+  // loop through first array and create object where properties === items in the array
+  // can we assume always 2 params?
+
+  let map = {};
+  for (let i = 0; i < arr1.length; i++) {
+    if (!map[arr1[i]]) {
+      const item = arr1[i];
+      map[item] = true;
+    }
+  }
+  console.log(map);
+  // loop through second array and check if item in second array exists on created object.
+  for (let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+containsCommonItem(array1, array2);
