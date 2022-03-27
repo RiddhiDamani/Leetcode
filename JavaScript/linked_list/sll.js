cf1bcbbe36d5ca75d5fea771056fe2da64d96d86
Riddhi Damani <riddhidamani90@gmail.com>
2022-03-26 21:57:02 -0500

    sll

--- JavaScript/linked_list/sll.js
+++ JavaScript/linked_list/sll.js
@@ -1,3 +1,8 @@
+// Singly Linked List
+// Node - piece of data - val
+//      - reference to next node - next
+
+// Node Definition
 class Node {
   constructor(val) {
     this.val = val;
@@ -5,4 +10,101 @@ class Node {
   }
 }
 
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
+  // push - insert at the end
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      console.log("this.tail:", this.tail);
+      console.log("this.tail.next:", this.tail.next);
+    }
+    this.length++;
+    return this;
+  }
+
+  // one way of traversing
+  // traverse() {
+  //   var current = this.head;
+  //   while (current) {
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+
+  // remove node from end of the Linked List
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    //console.log(current.val);
+    //console.log(newTail.val);
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  // remove a node from beginning of the linked list.
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    var oldHead = this.head;
+    this.head = oldHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return oldHead;
+  }
+
+  // add node at the beginning of the linked list.
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+var list = new SinglyLinkedList();
+list.push("HELLO");
+list.push("GOODBYE");
+list.push("!");
+//list.traverse();
+console.log(list);
+
+// var first = new Node("Hi");
+// first.next = new Node("There!");
+// first.next.next = new Node("How");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you?");
+
+// console.log(first);
